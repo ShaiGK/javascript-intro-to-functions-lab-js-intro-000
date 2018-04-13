a0040243dcae554ee2babf1c8fda93f103525136
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-13 00:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma (string) {
-  var string = string
+  var string = "hi"
   if (string === string.toLowerCase) {return "I can't hear you!"}
   else if (string === string.toUpperCase) {return "YES INDEED!"}
   else if (string = "I love you, Grandma.") {return "I love you, too."}
