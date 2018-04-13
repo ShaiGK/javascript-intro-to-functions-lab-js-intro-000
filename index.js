91a3e6dc10da5f7a39d37ea3ba587b9cdd418bb8
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-13 00:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma (string) {
-  if (string === string.toLowerCase) {alert ("I can't hear you!")}
-  else if (string === string.toUpperCase) {alert ("YES INDEED!")}
-  else if (string = "I love you, Grandma.") {alert ("I love you, too.")}
+  if (string === string.toLowerCase) {return "I can't hear you!"}
+  else if (string === string.toUpperCase) {return "YES INDEED!"}
+  else if (string = "I love you, Grandma.") {return "I love you, too."}
 }
